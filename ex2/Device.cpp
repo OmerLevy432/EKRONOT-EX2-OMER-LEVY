a7c971718e84cc704067d2ef1a4384db94f5dece
OmerLevy432 <olevy34@gmail.com>
2024-11-15 08:59:05 +0200

    finished device api

---
+++ ex2/Device.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "Device.h"
+
+
+std::string Device::getOS() const
+{
+	return this->os;
+}
+
+bool Device::isActive() const
+{
+	return this->active;
+}
+
+void Device::activate()
+{
+	this->active = true;
+}
+
+void Device::deactivate()
+{
+	this->active = false;
+}
+
+Device::Device(unsigned int id, DeviceType type, std::string os)
+{
+	this->id = id;
+	this->type = type;
+	this->os = os;
+}
--- ex2/Device.h
+++ ex2/Device.h
@@ -6,8 +6,8 @@
 	Those definitions are used for tests,                //
 	you can add your own, but please do not delete any.  //
 */														 //
-enum DeviceType {PHONE, PC, LAPTOP, TABLET};		     //
-													     //
+enum DeviceType { PHONE, PC, LAPTOP, TABLET };		     //
+//
 #define UbuntuOS "UbuntuLinux"                           //						
 #define RedHatOS "RedHatLinux"							 //
 #define MacOS "MacOS"									 //
@@ -16,13 +16,28 @@ enum DeviceType {PHONE, PC, LAPTOP, TABLET};		     //
 #define WINDOWS10 "Windows10"                            //
 #define WINDOWS11 "Windows11"                            //
 #define ANDROID "Android"                                //
-													     //
+														 //
 /// ///////////////////////////////////////////////////////
 
 
 class Device
 {
+private:
+	std::string os;
+	bool active;
+	unsigned int id;
+	DeviceType type;
 
+public:
+	// methods that don't change the artibutes of the object
+	std::string getOS() const;
+	bool isActive() const;
+	unsigned int getID() const;
+	DeviceType getType() const;
 
+	// methods that change the artibutes of the object
+	void activate();
+	void deactivate();
+	Device(const unsigned int id, const DeviceType type, const std::string os);
 };
 
