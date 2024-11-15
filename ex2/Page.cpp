37ca2b98d68d6494957a4fea98cb67fad4b9cf32
OmerLevy432 <olevy34@gmail.com>
2024-11-15 09:39:54 +0200

    finished Page methods

---
+++ ex2/Page.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include "Page.h"
+
+
+void Page::init()
+{
+	this->posts = "";
+	this->status = "";
+}
+
+void Page::clearPage()
+{
+	this->posts = "";
+}
+
+void Page::setStatus(std::string status)
+{
+	this->status = status;
+}
+
+void Page::addLineToPosts(std::string new_line)
+{
+	this->posts += "\n"; // goes down a line
+	this->posts += new_line;
+}
+
+std::string Page::getPosts() const
+{
+	return this->posts;
+}
+
+std::string Page::getStatus() const
+{
+	return this->status;
+}
+
